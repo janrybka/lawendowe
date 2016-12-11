51c2050ffcbccfc432d18b82af3603e8a5cec67b
Jan Rybka <Jan Rybka>
2016-12-11 17:57:07 +0100

    wersja 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import './App.css';
 import dialogPolyfill from 'dialog-polyfill';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import './App.css';
+import './img/plus_cursor.png';
+import * as query from './module/getData';
 
 class ImageCard extends Component {
+  // <ReactIntense src={this.props.fullUrl} thumbnail={this.props.thumbUrl}  caption={'caption'} title={'title'} />
+
+  componentDidMount() {
+    var element = document.querySelector('.mdl-cell--2-col #' + this.props.id);
+    window.Intense(element);
+  }
   render() {
     return (
       <div className="mdl-cell mdl-cell--2-col">
-        <div className="imgCard mdl-card" style={{ background: 'url(' + this.props.item + ') center / cover' }} onClick={() => this.props.onShowDialog()}>
-          <div className="mdl-card__title mdl-card--expand">
-          </div>
-          <div className="mdl-card__actions">
-          </div>
-        </div>
+        <div className='imgCard' id={this.props.id}
+          style={{ background: 'url(' + this.props.thumbUrl + ') center / cover' }}
+          data-image={this.props.fullUrl}
+          data-title={"My beach adventure"}
+          data-caption={"Thanks Sam, for the great picture"} />
       </div>
     );
   }
@@ -21,20 +28,47 @@ class ImageCard extends Component {
 class GalleryCard extends Component {
   render() {
     return (
-      <div className="mdl-cell mdl-cell--12-col">
+      <div className={(this.props.onHideGallery != null) ? "mdl-cell mdl-cell--12-col" : "mdl-cell mdl-cell--4-col" }>
         <div className="galleryCard mdl-card" style={{ background: 'url(' + this.props.item + ') center / cover' }}>
           <div className="mdl-card__title mdl-card--expand">
-            <h2 className="mdl-card__title-text" >Tytuł</h2>
+            <h2 className="mdl-card__title-text" ></h2>
           </div>
           <div className="mdl-card__actions">
             <span>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Mauris sagittis asdf as  asd fdasdfasdf sadf  asffdf dsaf a sf fasdf
             </span>
-            <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onClick={() => this.props.onShowDialog()} >
-              Zobacz galerię
-            </button>
+            {
+              (this.props.onShowGallery != null) ? (
+                <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onClick={() => this.props.onShowGallery()} >
+                  Zobacz galerię
+                </button>
+              ) : ""
+            }
           </div>
+          {
+            (this.props.onHideGallery != null) ? (
+              <div className="mdl-card__menu">
+                <button className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored mdl-button--icon" onClick={() => this.props.onHideGallery()}>
+                  <i className="material-icons">close</i>
+                </button>
+              </div>
+            ) : ""
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+class GalleryBackButton extends Component {
+  render() {
+    return (
+      <div className="mdl-cell mdl-cell--12-col">
+        <div className='imgCard'>
+          <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onClick={() => this.props.onHideGallery()} >
+            Wróć do listy
+        </button>
         </div>
       </div>
     );
@@ -86,48 +120,17 @@ class App extends Component {
     this.state = {
       choosenImg: null,
       selectedGallery: null,
-      galleries: [
-        {
-          id: 1, 
-          cover: 'http://lawendowelove.pl/img/general/lawenda.jpg',
-          folder: 'pole_lawendy',          
-          elements: [
-            'pole_1',
-            'uprawa_2015',
-            'uprawa_22015',
-            'uprawa_32015',
-            'pole_5',
-            'pole_7',
-            'pole_8',
-            'pole_9',
-            'pole_10b',
-            'pole_11',
-            'pole_12',
-          ]
-        },
-        {
-          id: 2, cover: 'http://lawendowelove.pl/img/general/lawenda.jpg',
-          elements: [
-            'uprawa_22015',
-            'pole_1',
-          ]
-        },
-        {
-          id: 3, cover: 'http://lawendowelove.pl/img/general/lawenda.jpg',
-          elements: [
-            'uprawa_2015',
-            'pole_1',
-          ]
-        },
-        {
-          id: 4, cover: 'http://lawendowelove.pl/img/general/lawenda.jpg',
-          elements: [
-            'uprawa_22015',
-            'uprawa_2015',
-          ]
-        },
-      ],
+      galleries: [],
     };
+    query.getData('./data/galleries.json',
+      (data) => {
+        this.setState({
+          galleries: data
+        });
+      },
+      (error) => {
+        alert(error);
+      })
   }
   zoomImage(img) {
     this.setState({
@@ -150,26 +153,40 @@ class App extends Component {
     });
   }
   render() {
-    var items = this.state.galleries.map((elem, idx) => (
-      <GalleryCard key={elem.id} item={elem.cover} onShowDialog={() => this.showGallery(elem)} />
-    ));
-    if (this.state.selectedGallery != null) {
-      var idx = this.state.galleries.indexOf(this.state.selectedGallery);
-      var detailImgs = this.state.selectedGallery.elements.map((elem, idx) => (
-        <ImageCard key={elem} item={elem} onShowDialog={() => this.zoomImage(elem)} />
+    var items = [];
+    if (this.state.selectedGallery == null) {
+      items = this.state.galleries.map((elem, idx) => (
+        <GalleryCard key={elem.id} item={elem.cover} onShowGallery={() => this.showGallery(elem)} onHideGallery={null} />
       ));
-      items.splice(idx+1, 0, detailImgs);
-    }
+    } else
+      if (this.state.selectedGallery != null) {
+        var sg = this.state.selectedGallery;
+        var idx = this.state.galleries.indexOf(this.state.selectedGallery);
+        var detailImgs = this.state.selectedGallery.elements.map((elem, idx) => (
+          <ImageCard key={elem} id={'ic' + this.state.selectedGallery.id + "_" + elem}
+            thumbUrl={this.baseAddress + '/' + this.state.selectedGallery.folder + '/' + elem + '_small.jpg'}
+            fullUrl={this.baseAddress + '/' + this.state.selectedGallery.folder + '/' + elem + '_big.jpg'}
+            onShowDialog={() => this.zoomImage(elem)} />
+        ));
+        items = [
+          <GalleryCard key={sg.id} item={sg.cover} onShowGallery={null} onHideGallery={() => this.hideGallery(sg)} />
+        ];
+        items.splice(1, 0, detailImgs);
+        items.push(
+          <GalleryBackButton key={"backButton"} onHideGallery={() => this.hideGallery(sg)} />
+        );
+        //items.splice(idx+1, 0, detailImgs);
+      }
     return (
       <div className="App">
         {this.state.choosenImg != null && <Dialog img={this.state.choosenImg} onClose={() => this.closeZoom()} />}
         <div>
           <ReactCSSTransitionGroup
-          transitionName="example"
-          transitionEnterTimeout={500}
-          transitionLeaveTimeout={300} className="mdl-grid">
-          {items}
-        </ReactCSSTransitionGroup>
+            transitionName="example"
+            transitionEnterTimeout={300}
+            transitionLeaveTimeout={200} className="mdl-grid">
+            {items}
+          </ReactCSSTransitionGroup>
         </div>
       </div>
     );
